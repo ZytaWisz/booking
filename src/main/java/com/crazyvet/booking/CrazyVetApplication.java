284a058a591122deec04f2212b8872a6756fe2d1
Zyta Wiszniewska <Zyta.Wiszniewska@redwood.com>
2024-01-31 17:39:05 +0100

    my first commit

---
+++ src/main/java/com/crazyvet/booking/CrazyVetApplication.java
@@ -0,0 +1,13 @@
+package com.crazyvet.booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrazyVetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrazyVetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crazyvet/booking/CrazyVetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crazyvet.booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrazyVetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
